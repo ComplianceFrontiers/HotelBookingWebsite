d00b7d8f4c1701d68c63ade516723b4c566e934f
ramya72002 <nsriramya7@gmail.com>
2024-10-28 13:17:11 +0530

    things done

--- pages/select-single/RoomSingleSection/Rcomponents/descontent/pricing.js
+++ pages/select-single/RoomSingleSection/Rcomponents/descontent/pricing.js
@@ -36,8 +36,8 @@ const Pricing = (props) => {
             </div>
             
             <div className="map-area">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.9147703055!2d-74.11976314309273!3d40.69740344223377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbd!4v1547528325671"></iframe>
-            </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3066.645814744197!2d-75.495459!3d39.77006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6e31a86d581cd%3A0xb25103ed4aea7e7f!2s510%20Duncan%20Rd%2C%20Wilmington%2C%20DE%2019809%2C%20USA!5e0!3m2!1sen!2sin!4v1730101244181!5m2!1sen!2sin"></iframe>                </div>
+        
         </div>
     )
 }

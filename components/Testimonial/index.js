f2de021712d8b901814d80a5ccd8cc55f012f5bb
ramya72002 <nsriramya7@gmail.com>
2024-11-04 16:24:01 +0530

    notification implemented

--- components/Testimonial/index.js
+++ components/Testimonial/index.js
@@ -54,19 +54,19 @@ const Testimonial = () => {
             tsImg: ts1,
             Des: "“The perfect venue for our community event! The facilities were spotless, and the additional services like A/V equipment made our presentation seamless. Highly recommend!”",
             Title: 'Sarah L., Newark, DE',
-            Sub: "Us",
+            Sub: "Newark",
         },
         {
             tsImg: ts2,
             Des: "“We hosted a family reunion at the pavilion, and it was a fantastic experience. The setup and cleanup services allowed us to focus on spending quality time with loved ones.”",
             Title: 'Mark R., Wilmington, DE',
-            Sub: "Us",
+            Sub: "Dover",
         },
         {
             tsImg: ts3,
             Des: "“From the spacious multi-purpose room to the friendly on-site support, everything exceeded our expectations. Bellevue Community Center made our event unforgettable!”",
             Title: 'Emily K., Dover, DE',
-            Sub: "Us",
+            Sub: "Wilmington",
         }
         
     ]

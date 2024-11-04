21142f7cbb344368aed0bfedc8fb542b8348f507
ramya72002 <nsriramya7@gmail.com>
2024-11-04 16:47:51 +0530

    notification implemented

--- components/footer/index.js
+++ components/footer/index.js
@@ -31,7 +31,7 @@ const Footer = (props) => {
                                 <ul>
                                 <li><i className="fi ti-location-pin"></i>510 Duncan Rd, Wilmington, DE, United States, Delaware</li>
                                 <li><i className="fi flaticon-email"></i>info@bellevuecc.org</li>
-                                <li><i className="fi flaticon-phone"></i>Contact : (302) 762 - 1391</li>
+                                <li><i className="fi flaticon-phone">📞  </i>Contact : (302) 762 - 1391</li>
                                 </ul>
                                 </div>
                             </div>
